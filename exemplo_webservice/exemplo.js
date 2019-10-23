0e4f4176bcd7a50d03b5e837989a43ad9ee4ed35
João Pedro Mariotti <mariotti660@gmail.com>
2019-10-23 19:39:40 -0300

    exemplo

---
+++ exemplo_webservice/exemplo.js
@@ -0,0 +1,44 @@
+function carregar_dados(){
+	var dados = {
+		metodo: "select"
+	}
+
+	$.post("http://localhost/aula6_exemplo/api.php", dados, function(data){
+		var json = JSON.parse(data);
+		$("#resultado").append(data);
+		for(var i=0; i<json.length; i++){
+			$("#resultado").append(json[i].id + "<br>");
+		}
+	});
+}
+$( window ).load(function() {
+  carregar_dados();
+});
+function  buscar_usuario()
+{	
+
+	var input_valor = $("[name=input_valor]").val();
+
+  	var dados = {
+		metodo: "mostrar", valor: input_valor  
+	}
+
+	$.post("http://localhost/aula6_exemplo/api.php", dados, function(data){
+	var json = JSON.parse(data);
+		for(var i=0; i<json.length; i++){
+			$("#resultado").append(json[i].id + json[i].nome + json[i].email + json[i].senha + "<br>");
+		}
+	})
+	.done(function(){
+          alert("Ok");
+     })
+	 .fail(function(){
+          alert("Erro :");
+     });
+}	
+    
+$("#btn_buscar").on('click', function()
+    {
+       buscar_usuario();
+
+    });
