5767fcba0ee490b2061f89da903eb3048a71c163
João Pedro Mariotti <mariotti660@gmail.com>
2019-11-20 20:10:25 -0300

    ajuste compra

--- views/site/produto.php
+++ views/site/produto.php
@@ -1,4 +1,7 @@
 <?php
+	if(session_status() <> PHP_SESSION_ACTIVE)
+		session_start();
+	
 	require_once('../../controllers/produtos.php');
 	require_once('../../controllers/avaliacaos.php');
 	require_once('../../controllers/pedidos.php');
